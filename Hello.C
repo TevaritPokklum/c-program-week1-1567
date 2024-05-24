5d986b68d59096efc5ad90ec01870a3cde42c758
TevaritPokklum <thewvththipkkhium@gmail.com>
2024-05-24 16:19:20 +0700

    week-1-c-program

---
+++ Hello.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    //Create integer variable
+    int age = 16;
+    printf("hello world!\n");
+    printf("My Name is two \n");
+    printf("Age: %d\n",age);
+    return 0;
+}
